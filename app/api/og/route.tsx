bcef8f2f573d2207660ac6793f1d58ba3a12dff0
Jonathan Gamble <jdgamble555@gmail.com>
2025-11-28 21:38:32 -0600

    next cloudflare

---
+++ app/api/og/route.tsx
@@ -0,0 +1,41 @@
+import { type NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const parameters = Object.fromEntries(searchParams.entries());
+
+  return new ImageResponse(
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#6f90ab",
+        fontSize: "2rem",
+        color: "#fff",
+      }}
+    >
+      <span>Noto Sans (Default Font)</span>
+      <span
+        style={{
+          fontFamily: "JetBrains Mono",
+        }}
+      >
+        JetBrains Mono (using GoogleFont class)
+      </span>
+      <span>These are emojis: 😎🌩️</span>
+      <span
+        style={{
+          fontFamily: "JetBrains Mono",
+        }}
+      >
+        Parameters: {JSON.stringify(parameters)}
+      </span>
+    </div>
+  );
+}
